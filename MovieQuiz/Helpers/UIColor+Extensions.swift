7d2575991c741ed304b8186972bb1707383682f0
anika <Anna.Fedchenko2002@yandex.ru>
2024-06-21 18:24:09 +0300

    created project

---
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
+    static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+    static var ypGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
+}
